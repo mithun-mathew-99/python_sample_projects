3b4a95729502dc42bcd57b5b45f70074254c7126
Mithun <mmathew1355@gmail.com>
2025-09-15 06:03:36 +0530

    updated unit_converter.py to add loop

--- Fourth/unit_converter.py
+++ Fourth/unit_converter.py
@@ -1,40 +1,41 @@
 def km_to_miles(km):
-  retrun km * 0.621371
+    return km * 0.621371
 
 def miles_to_km(miles):
-  retrun miles/0.621371
+    return miles / 0.621371
 
 def c_to_f(celsius):
-  return (celsius*9/5) + 32
+    return (celsius * 9 / 5) + 32
 
 def f_to_c(fahrenheit):
-  return (fahrenheit -32)*5/9
+    return (fahrenheit - 32) * 5 / 9
 
-print(" Unit Converter")
+print("Unit Converter")
 print("1. Kilometers to Miles")
 print("2. Miles to Kilometers")
 print("3. Celsius to Fahrenheit")
 print("4. Fahrenheit to Celsius")
-choice = int(input("Choose conversion (1 - 4)"))
+print("5. Exit")
 
-try:
-  if choise == 1:
-    km = float(input("Enter Kilometers : ")
-    print(f" {km} km = {km_to_miles(km) : .2f} miles")
-
-  if choise == 2:
-    miles = float(input("Enter Miles : ")
-    print(f" {miles} miles = {miles_to_km(miles) : .2f} km")
-
-  if choise == 3:
-    celsius = float(input("Enter temperature in Celsius : ")
-    print(f" {km} \u00bo Celcius = {km_to_miles(celsius) : .2f} Fahrenheit")
-
-  if choise == 4:
-    fahrenheit = float(input("Enter temperature in Fahrenheit : ")
-    print(f" {fahrenheit} Fahrenheit = {f_to_c(fahrenheit) : .2f} \u00bo Celcius")
-
-  else:
-    print("Invalid input")
-except ValueError:
-  print(" please enter a valid number")
+while True:
+    try:
+        choice = int(input("Choose conversion (1 - 5): "))
+        if choice == 1:
+            km = float(input("Enter Kilometers: "))
+            print(f"{km} km = {km_to_miles(km):.2f} miles\n")
+        elif choice == 2:
+            miles = float(input("Enter Miles: "))
+            print(f"{miles} miles = {miles_to_km(miles):.2f} km\n")
+        elif choice == 3:
+            celsius = float(input("Enter temperature in Celsius: "))
+            print(f"{celsius}° Celsius = {c_to_f(celsius):.2f} Fahrenheit\n")
+        elif choice == 4:
+            fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+            print(f"{fahrenheit} Fahrenheit = {f_to_c(fahrenheit):.2f}° Celsius\n")
+        elif choice == 5:
+            print("Exiting Unit Converter. Goodbye!")
+            break
+        else:
+            print("Invalid input\n")
+    except ValueError:
+        print("Please enter a valid number")
